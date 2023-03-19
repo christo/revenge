caba4d7f90272c0081162f3a3045aa0b22eddf61
Chris Mountford <chris@think.net.au>
2023-03-19 16:14:59 +1100

    added some tests for BinUtils

---
+++ client/src/misc/BinUtils.test.ts
@@ -0,0 +1,22 @@
+import {unsignedToSigned} from "./BinUtils";
+
+
+/** Data-driven test of two's complement: input, expected output */
+const TC_DATA:[number,number][] = [
+    [8,8],
+    [127,127],
+    [0,0],
+    [0xf2, -14],
+    [0xff,-1],
+    [128,-128],
+    [0b11110010, -14],
+    [0b11011011, -37],
+    [0b10011100, -100]
+];
+
+test('twos complement', () => {
+    TC_DATA.forEach(x => expect(unsignedToSigned(x[0])).toBe(x[1]));
+});
+
+
+export {}
--- client/src/misc/BinUtils.ts
+++ client/src/misc/BinUtils.ts
@@ -1,8 +1,9 @@
 
-export const hex16 = (x: number) => (0xffff & x).toString(16).padStart(4, "0").toLowerCase();
-export const hex8 = (x: number) => (0xff & x).toString(16).padStart(2, "0").toLowerCase();
+const hex16 = (x: number):string => (0xffff & x).toString(16).padStart(4, "0").toLowerCase();
+const hex8 = (x: number):string => (0xff & x).toString(16).padStart(2, "0").toLowerCase();
 
-export const stringToArray = (s: string) => {
+/** Returns the given string as an array of char codes */
+const stringToArray = (s: string):number[] => {
     const prefix = [];
     for (let i = 0; i < s.length; i++) {
         prefix.push(s.charCodeAt(i));
@@ -10,9 +11,20 @@ export const stringToArray = (s: string) => {
     return prefix;
 }
 
-export const assertByte = (value: number) => {
+/** Returns the given number after asserting it is in byte range. */
+const assertByte = (value: number):number => {
     if (value < 0 || value > 255) {
         throw Error("expecting an unsigned byte value (was " + value + ")");
     }
     return value & 0xff;
 };
+
+/**
+ * Takes a byte value in the range 0-255 and interprets its numeric value as an 8 bit two's complement value
+ * between -128 and 127.
+ *
+  * @param x 8 bit signed byte value
+ */
+const unsignedToSigned = (x:number):number => -(x & 0x80) + (x & 0x7f)
+
+export {unsignedToSigned, assertByte, stringToArray, hex16, hex8}
