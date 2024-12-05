ec7b233afb82025086c52c5f000a586a54e7a234
Chris Mountford <chris.mountford@gmail.com>
2024-12-05 19:41:51 +1100

    wip test for tracer

--- client/src/machine/tracer.test.ts
+++ client/src/machine/tracer.test.ts
@@ -1,27 +1,32 @@
 import {Tracer} from "./tracer";
-import {Disassembler, DisassemblyMetaImpl, SymbolTable} from "./asm";
+import {Disassembler, DisassemblyMetaImpl, JumpTargetFetcher, LabelsComments, SymbolTable} from "./asm";
 import {Mos6502} from "./mos6502";
 import {FileBlob} from "./FileBlob";
-import {ArrayMemory, LE} from "./core";
+import {Addr, ArrayMemory, LE} from "./core";
 
 function mem(contents: number[]) {
     return new ArrayMemory(contents, LE, true, true);
 }
-/*
 
-test('simple linear trace', () => {
+const fakeJumpTargetFetcher: JumpTargetFetcher = (fb: FileBlob) => {
+    return [[123, new LabelsComments()]]
+}
+
+/**
+ * WIP TODO
+ */
+test.skip('simple linear trace', () => {
     const i = Mos6502.INSTRUCTIONS;
-    const machineCode = i.builder()
-        .brk()
+    const machineCode = Mos6502.builder()
+        .opMap.brk([0]) // TODO hack todo
         .build();
     const fb = new FileBlob("testblob", machineCode, LE);
     const st = new SymbolTable("empty");
-    const jtf =
+    const jtf: JumpTargetFetcher = fakeJumpTargetFetcher;
     const dm = new DisassemblyMetaImpl(0, 0, 0, [], jtf, st);
     const d = new Disassembler(i, fb, dm);
     const t = new Tracer(d, 0, mem(machineCode));
     expect(t.running()).toBe(true);
 });
-*/
 
 export {};
