516f199134e06589074728e2bacdf28edab75acf
Chris Mountford <chris.mountford@gmail.com>
2024-12-20 14:26:57 +1100

    test disassembling JMP $6502

--- client/test/machine/asm/Disassembler.test.ts
+++ client/test/machine/asm/Disassembler.test.ts
@@ -1,10 +1,7 @@
 import {expect} from 'chai';
-import {Disassembler} from "../../../src/machine/asm/Disassembler";
-import {Mos6502} from "../../../src/machine/mos6502";
-import {FileBlob} from "../../../src/machine/FileBlob";
 import {ArrayMemory, LE} from "../../../src/machine/core";
-import {DisassemblyMetaImpl} from "../../../src/machine/asm/DisassemblyMetaImpl";
-import {niladicOpcodes} from "../util";
+import {createDisassembler, niladicOpcodes} from "../util";
+import {AddressingMode, MODE_ABSOLUTE, Mos6502} from "../../../src/machine/mos6502";
 
 describe("disassembler", () => {
   it("disassemble1 niladic", () => {
@@ -17,5 +14,21 @@ describe("disassembler", () => {
     const disassembled = d.disassemble1(mem, 2);
     console.log(disassembled);
     expect(disassembled.instruction.op.mnemonic).to.equal("NOP");
-  })
+  });
+  it("disassembles single JMP instruction", () => {
+    const bytes = [
+        0x4c, 0x02, 0x65  // JMP $6502
+    ];
+    const code: number[] = [0, 0, ...bytes];
+    const mem = new ArrayMemory(code, LE, true, true);
+    const d = createDisassembler(bytes, 2);
+    const disassembled = d.disassemble1(mem, 2);
+    const instruction = disassembled.instruction;
+    const op = instruction.op;
+    expect(instruction.op.isJump).to.equal(true);
+    expect(op.mnemonic).to.equal("JMP");
+    expect(instruction.mode).to.equal(MODE_ABSOLUTE);
+    expect(disassembled.getBytes().length).to.equal(3);
+    expect(disassembled.operand16()).to.equal(0x6502);
+  });
 })
