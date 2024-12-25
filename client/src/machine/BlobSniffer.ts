3c5651293b79a34bccc2f9cda553fa680fd3f7b1
Chris Mountford <chris.mountford@gmail.com>
2024-12-26 09:30:20 +1100

    tidy

--- client/src/machine/BlobSniffer.ts
+++ client/src/machine/BlobSniffer.ts
@@ -11,20 +11,20 @@ interface BlobSniffer {
   tags: string[];
 
   /**
-   * Produces a score for the given FileBlob, higher numbers indicate a corresponding higher confidence of
-   * a match. Values should be coefficients so that an aggregate score is achieved by multiplication. A
-   * value of 1 signifies no indication of a match, less than 1 signifies unlikeliness and greater than 1
+   * Produces a score for the given FileBlob, higher numbers indicate a corresponding higher confidence
+   * of a match. Component values are coefficients that multiply to produce aggregate scores. A value
+   * of 1 signifies no indication of a match, less than 1 signifies unlikeliness and greater than 1
    * signifies increasing confidence. Zero is absolute certainty. Negative values must not be returned.
+   *
    * @param fb the file contents to sniff
    */
   sniff(fb: FileBlob): number;
 
   /**
-   * This smells, it's a bag of disassembly-specific detail transported from the thing that knows about the
-   * file contents and the disassembler who needs to construct the {@link DataView}. Is there a generic
-   * way to bundle this stuff? What's the common API such that ignorant intermediaries can be blissfull
-   * as they work at a non-specific altitude? Consider an inversion as I did with the Dialect API. Or
-   * use generics.
+   * FUTURE: This smells, it's a bag of disassembly-specific detail transported from the thing that knows
+   *  about the file contents and the disassembler who needs to construct the {@link DataView}. Is there
+   *  a generic way to bundle this stuff? What's the common API such that ignorant intermediaries can be
+   *  blissfull as they work at a non-specific altitude? Consider an inversion or use generics.
    */
   getMeta(): DisassemblyMeta;
 }
