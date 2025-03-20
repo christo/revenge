09957713297468e78d235f79d79b5860ccee9677
Chris Mountford <chris.mountford@gmail.com>
2025-03-20 13:33:00 +1100

    notes

--- client/src/machine/DataView.ts
+++ client/src/machine/DataView.ts
@@ -3,12 +3,21 @@ import {LogicalLine} from "./api.ts";
 /**
  * Representation of a generic view of data, a vertical sequence of horizontal string of kv pairs.
  * A generic displayable structure with a sequence of {@link LogicalLine LogicalLines}.
+ *
+ * Examples supported should include comments, commented out code, lines with possibly multiple labels,
+ * hex dumps, data with alternate views (petscii, code, graphics etc.)
+ * Macro definitions are an especially important feature to get right, they
+ * are not explicit in the binary and their introduction during reversing is dynamic and speculative.
+ *
  */
 interface DataView {
   getLines(): LogicalLine[];
 
   addLine(ll: LogicalLine): void;
 }
+
+// TODO a data view probably needs to have view items that are not mapped to logical lines
+//   (think this through) there may need to be decorative content I haven't thought clearly enough about
 
 class DataViewImpl implements DataView {
   private readonly lines: LogicalLine[];
