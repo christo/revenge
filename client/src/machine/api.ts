8bb9e90e877531e36ca0594e7b67c54e6c4380e0
Chris Mountford <chris.mountford@gmail.com>
2025-03-20 13:31:10 +1100

    duplicate todo

--- client/src/machine/api.ts
+++ client/src/machine/api.ts
@@ -81,7 +81,6 @@ class LogicalLine {
   /**
    * Temporary transition encapsulation
    *
-   * TODO: use an abstraction that more closely models the domain, tags carry too much responsibility
    */
   private readonly tags: Tag[];
   private readonly address: Addr;
