54dd45b185fcf79fde9a1c959dcb32abaf6fe328
Chris Mountford <chris.mountford@gmail.com>
2025-03-21 16:09:06 +1100

    remove InfoPanel.css

--- client/src/ui/InfoPanel.tsx
+++ client/src/ui/InfoPanel.tsx
@@ -3,18 +3,25 @@ import Typography from "@mui/material/Typography";
 
 import {Detail} from "./Detail.ts";
 import {SmolPanel} from "./SmolPanel.tsx";
-import './InfoPanel.css';
+
+const STYLE = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "flex-start",
+  w: "100%",
+  mb: 1,
+};
 
 export function InfoPanel({detail}: { detail: Detail }) {
-  return <Box className="infopanel">
+  return <Box sx={STYLE}>
     <SmolPanel heading="Options">
-      <Typography>Options control panel will go here...</Typography>
+      <Typography>Control panel will go here...</Typography>
     </SmolPanel>
     <SmolPanel heading={detail.name}>
       {detail.stats.map(([key, value], i) =>
           <Box key={`sp_${i}`} className="stat">
-            <Typography display="inline" sx={{opacity: 0.7}}>{key}</Typography>
-            <Typography display="inline" sx={{color: "#9bd2cd"}}>{value}</Typography>
+            <Typography display="inline" sx={{opacity: 0.7, mr: 1}}>{key}</Typography>
+            <Typography display="inline" sx={{color: "#e3e0bd", fontWeight: "bold"}}>{value}</Typography>
           </Box>
       )}
     </SmolPanel>
