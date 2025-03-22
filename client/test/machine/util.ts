5c7a4b164290cee44caaa377a428632db620bdf6
Chris Mountford <chris.mountford@gmail.com>
2025-03-22 11:26:49 +1100

    remove dead code

--- client/test/machine/util.ts
+++ client/test/machine/util.ts
@@ -1,8 +1,5 @@
 import {Mos6502} from "../../src/machine/mos6502";
 import {LE} from "../../src/machine/core";
-import {FileBlob} from "../../src/machine/FileBlob";
-import {DisassemblyMetaImpl} from "../../src/machine/asm/DisassemblyMetaImpl";
-import {Disassembler} from "../../src/machine/asm/Disassembler";
 import {ArrayMemory} from "../../src/machine/Memory";
 
 /**
@@ -20,15 +17,4 @@ export function mem(contents: number[], offset: number = 0) {
     throw Error("not implemented");
   }
   return arrayMemory;
-}
-
-/**
- * Convenience for creating a little endian disassembler with base address at 0 and content at 2
- * @param bytes program including load address in little endian at address 0, 1
- * @deprecated
- */
-export function createDisassembler(bytes: number[]) {
-  const fb = FileBlob.fromBytes("testblob", bytes, LE);
-  const dm = new DisassemblyMetaImpl(0, 0, 2);
-  return new Disassembler(Mos6502.ISA, fb, dm);
 }
