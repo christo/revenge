894b8aae103edd198905c84b673d0ced93cd00f6
Chris Mountford <chris.mountford@gmail.com>
2025-03-29 22:30:11 +1100

    tone down the yellow

--- client/src/neonColourScheme.ts
+++ client/src/neonColourScheme.ts
@@ -7,7 +7,7 @@ import { createTheme } from "@mui/material";
 export const darkPurple = "#1B0420";
 export const neonPink = "#E346A5";
 export const neonCyan = "#0AE0E3";
-export const neonYellow = "#f8ff22";
+export const neonYellow = "#ccd044";
 
 // TODO more accent colour variants
 // TODO a less vivid main text colour
