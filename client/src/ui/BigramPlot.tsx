c53488172923962ebf6c790f0c71dff45bcc1500
Chris Mountford <chris.mountford@gmail.com>
2025-03-30 18:18:05 +1100

    plot with background colour

--- client/src/ui/BigramPlot.tsx
+++ client/src/ui/BigramPlot.tsx
@@ -2,17 +2,17 @@ import { Box } from "@mui/material";
 import {Bigram} from "../analysis/Bigram.ts";
 import {FileBlob} from "../machine/FileBlob.ts";
 import {useEffect, useRef } from "react";
+import {background} from "../neonColourScheme.ts";
 
 
 function intensity(value: number, max: number) {
   const linear = value / max;
-  // const normalised = linear ; // 0 - 1
-  const normalised = Math.pow(linear, 1/4); // 0 - 1
+  const normalised = Math.pow(linear, 1/3); // 0 - 1
   const result = (normalised * 255).toFixed(0);
   return `rgb(${result},${result},${result})`;
 }
 
-function bigrams(canvas: HTMLCanvasElement, fb: FileBlob) {
+function bigrams(canvas: HTMLCanvasElement, fb: FileBlob, bgColor: string) {
 
   const bigram = new Bigram(fb);
   const max = bigram.getMax();
@@ -23,16 +23,18 @@ function bigrams(canvas: HTMLCanvasElement, fb: FileBlob) {
   if (context) {
 
     // start with black
-    context.fillStyle = "#000";
+    context.fillStyle = bgColor;
     context.fillRect(0, 0, canvas.width, canvas.height);
     // draw bigram value plot
 
     bigram.forEach((first, second, value) => {
       // context.fillStyle = hsl1FillStyle(value, max);
-      context.fillStyle = intensity(value, max);
+      if (value !== 0) {
+        context.fillStyle = intensity(value, max);
 
-      // first will be x, second will be y
-      context.fillRect(first*2, second*2, 2, 2);
+        // first will be x, second will be y
+        context.fillRect(first*2, second*2, 2, 2);
+      }
     });
   }
 
@@ -43,7 +45,7 @@ function BigramPlot({fb}: {fb: FileBlob}) {
   useEffect(() => {
     if (canvasRef.current) {
       console.log("about to render bigram on canvas")
-      bigrams(canvasRef.current, fb);
+      bigrams(canvasRef.current, fb, background);
     }
   }, [fb]);
 
