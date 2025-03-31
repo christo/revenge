58026ecb52f29e15822e66ec015380decce5b0a3
Chris Mountford <chris.mountford@gmail.com>
2025-03-31 17:28:10 +1100

    consistency

--- client/test/machine/cbm/cbm.test.ts
+++ client/test/machine/cbm/cbm.test.ts
@@ -10,8 +10,8 @@ import {Mos6502} from "../../../src/machine/mos6502";
 
 describe("disassembler integration", () => {
   it("loads hesmon", () => {
-    const hesmon = fs.readFileSync("../server/data/preload/HesMon.prg");
-    const fb = FileBlob.fromBytes("hesmon", Array.from(hesmon), Mos6502.ENDIANNESS);
+    const f = fs.readFileSync("../server/data/preload/HesMon.prg");
+    const fb = FileBlob.fromBytes("hesmon", Array.from(f), Mos6502.ENDIANNESS);
     const dm = new DisassemblyMetaImpl(0, [[0, "test"]], 2);
     const d = new Disassembler(Mos6502.ISA, fb, dm);
     const lines: InstructionLike[] = [];
