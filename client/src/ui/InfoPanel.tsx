d0b7330d9a786d9b90bd8acd25b12edaa05a8a90
Chris Mountford <chris.mountford@gmail.com>
2025-04-03 23:02:38 +1100

    don't need headings for these sections

--- client/src/ui/InfoPanel.tsx
+++ client/src/ui/InfoPanel.tsx
@@ -2,7 +2,6 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 import {Detail} from "./Detail.ts";
-import {SmolPanel} from "./SmolPanel.tsx";
 
 const STYLE = {
   display: "flex",
@@ -10,20 +9,21 @@ const STYLE = {
   alignItems: "flex-start",
   w: "100%",
   mb: 2,
+  gap: 4
 };
 
 export function InfoPanel({detail}: { detail: Detail }) {
   return <Box sx={STYLE}>
-    <SmolPanel heading={detail.name}>
+    <Box>
       {detail.stats.map(([key, value], i) =>
           <Box key={`sp_${i}`} className="stat">
             <Typography display="inline" sx={{opacity: 0.7, mr: 1}}>{key}</Typography>
             <Typography display="inline" sx={{color: "#e3e0bd", fontWeight: "bold"}}>{value}</Typography>
           </Box>
       )}
-    </SmolPanel>
-    <SmolPanel heading="Options">
+    </Box>
+    <Box>
       <Typography>Control panel will go here...</Typography>
-    </SmolPanel>
+    </Box>
   </Box>;
 }
--- client/src/ui/SmolPanel.tsx
+++
@@ -1,9 +0,0 @@
-import {Box, Typography} from "@mui/material";
-import {ReactNode} from "react";
-
-export function SmolPanel({heading, children}: { heading: string, children: ReactNode }) {
-  return <Box sx={{pr: 2}}>
-    <Typography variant="h6">{heading}</Typography>
-    {children}
-  </Box>;
-}
