03f9bddd5b6dc4ee527bf234436b4d570e3d839b
Chris Mountford <chris.mountford@gmail.com>
2025-04-03 15:05:08 +1100

    reenable strict mode for now

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
-    // <React.StrictMode> // TODO test with strict mode
+    <React.StrictMode>
     <App/>
-    // </React.StrictMode>
+    </React.StrictMode>
 );
