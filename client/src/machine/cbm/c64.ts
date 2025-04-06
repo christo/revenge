970b319e15fcd7e4156b05a67a173b36e9e90270
Chris Mountford <chris.mountford@gmail.com>
2025-04-06 15:24:15 +1000

    naming of C64 symbol table consistent with vic (and its true meaning)

--- client/src/machine/cbm/c64.ts
+++ client/src/machine/cbm/c64.ts
@@ -88,46 +88,46 @@ const jumpTargetFetcher: SymbolResolver = (fb: FileBlob) => [
 /**
  * C64 Kernal routines as SymbolTable
  */
-const C64_KERNAL = new SymbolTable("c64");
-C64_KERNAL.sub(0xffa5, "acptr", "Input byte from serial port");
-C64_KERNAL.sub(0xffc6, "chkin", "Open channel for input");
-C64_KERNAL.sub(0xffc9, "chkout", "Open channel for output");
-C64_KERNAL.sub(0xffcf, "chrin", "Input character from channel");
-C64_KERNAL.sub(0xffd2, "chrout", "Output character to channel");
-C64_KERNAL.sub(0xffa8, "ciout", "Transmit a byte over the serial bus");
-C64_KERNAL.sub(0xff81, "lint", "Initialize screen editor");
-C64_KERNAL.sub(0xffe7, "clall", "Close all channels and files");
-C64_KERNAL.sub(0xffc3, "close", "Close a specified logical file");
-C64_KERNAL.sub(0xffcc, "clrchn", "Close input and output channels");
-C64_KERNAL.sub(0xffe4, "getin", "Get character from keyboard buffer");
-C64_KERNAL.sub(0xfff3, "iobase", "Return base address of I/O devices");
-C64_KERNAL.sub(0xff84, "ioinit", "Initialize input/output");
-C64_KERNAL.sub(0xffb1, "listen", "Command devices on serial bus to LISTEN");
-C64_KERNAL.sub(0xffd5, "load", "Load RAM from a device");
-C64_KERNAL.sub(0xff9c, "membot", "Read/set bottom of memory");
-C64_KERNAL.sub(0xff99, "memtop", "Read/set top of memory");
-C64_KERNAL.sub(0xffc0, "open", "Open a logical file");
-C64_KERNAL.sub(0xfff0, "plot", "Read/set X,Y cursor position");
-C64_KERNAL.sub(0xff87, "ramtas", "Initialize RAM, reset tape buffer");
-C64_KERNAL.sub(0xffde, "rdtim", "Read realtime clock");
-C64_KERNAL.sub(0xffb7, "readst", "Read I/O status word");
-C64_KERNAL.sub(0xff8a, "restor", "Restore I/O default vectors");
-C64_KERNAL.sub(0xffd8, "save", "Save RAM to device");
-C64_KERNAL.sub(0xff9f, "scnkey", "Scan keyboard");
-C64_KERNAL.sub(0xffed, "screen", "Return X,Y organization of screen");
-C64_KERNAL.sub(0xff93, "second", "Send secondary address after LISTEN");
-C64_KERNAL.sub(0xffba, "setlfs", "Set logical, first, and second address");
-C64_KERNAL.sub(0xff90, "setmsg", "Control Kernal messages");
-C64_KERNAL.sub(0xffbd, "setnam", "Set filename");
-C64_KERNAL.sub(0xffdb, "settim", "Set realtime clock");
-C64_KERNAL.sub(0xffa2, "settmo", "Set time-out on serial bus");
-C64_KERNAL.sub(0xffe1, "stop", "Check for STOP key");
-C64_KERNAL.sub(0xffb4, "talk", "Command serial bus device to TALK");
-C64_KERNAL.sub(0xff96, "tksa", "Send secondary address after TALK");
-C64_KERNAL.sub(0xffea, "udtim", "Increment realtime clock");
-C64_KERNAL.sub(0xffae, "unlsn", "Command serial bus to UNLISTEN");
-C64_KERNAL.sub(0xffab, "untlk", "Command serial bus to UNTALK");
-C64_KERNAL.sub(0xff8d, "vector", "Read/set vectored I/O");
+const C64_SYM = new SymbolTable("c64");
+C64_SYM.sub(0xffa5, "acptr", "Input byte from serial port");
+C64_SYM.sub(0xffc6, "chkin", "Open channel for input");
+C64_SYM.sub(0xffc9, "chkout", "Open channel for output");
+C64_SYM.sub(0xffcf, "chrin", "Input character from channel");
+C64_SYM.sub(0xffd2, "chrout", "Output character to channel");
+C64_SYM.sub(0xffa8, "ciout", "Transmit a byte over the serial bus");
+C64_SYM.sub(0xff81, "lint", "Initialize screen editor");
+C64_SYM.sub(0xffe7, "clall", "Close all channels and files");
+C64_SYM.sub(0xffc3, "close", "Close a specified logical file");
+C64_SYM.sub(0xffcc, "clrchn", "Close input and output channels");
+C64_SYM.sub(0xffe4, "getin", "Get character from keyboard buffer");
+C64_SYM.sub(0xfff3, "iobase", "Return base address of I/O devices");
+C64_SYM.sub(0xff84, "ioinit", "Initialize input/output");
+C64_SYM.sub(0xffb1, "listen", "Command devices on serial bus to LISTEN");
+C64_SYM.sub(0xffd5, "load", "Load RAM from a device");
+C64_SYM.sub(0xff9c, "membot", "Read/set bottom of memory");
+C64_SYM.sub(0xff99, "memtop", "Read/set top of memory");
+C64_SYM.sub(0xffc0, "open", "Open a logical file");
+C64_SYM.sub(0xfff0, "plot", "Read/set X,Y cursor position");
+C64_SYM.sub(0xff87, "ramtas", "Initialize RAM, reset tape buffer");
+C64_SYM.sub(0xffde, "rdtim", "Read realtime clock");
+C64_SYM.sub(0xffb7, "readst", "Read I/O status word");
+C64_SYM.sub(0xff8a, "restor", "Restore I/O default vectors");
+C64_SYM.sub(0xffd8, "save", "Save RAM to device");
+C64_SYM.sub(0xff9f, "scnkey", "Scan keyboard");
+C64_SYM.sub(0xffed, "screen", "Return X,Y organization of screen");
+C64_SYM.sub(0xff93, "second", "Send secondary address after LISTEN");
+C64_SYM.sub(0xffba, "setlfs", "Set logical, first, and second address");
+C64_SYM.sub(0xff90, "setmsg", "Control Kernal messages");
+C64_SYM.sub(0xffbd, "setnam", "Set filename");
+C64_SYM.sub(0xffdb, "settim", "Set realtime clock");
+C64_SYM.sub(0xffa2, "settmo", "Set time-out on serial bus");
+C64_SYM.sub(0xffe1, "stop", "Check for STOP key");
+C64_SYM.sub(0xffb4, "talk", "Command serial bus device to TALK");
+C64_SYM.sub(0xff96, "tksa", "Send secondary address after TALK");
+C64_SYM.sub(0xffea, "udtim", "Increment realtime clock");
+C64_SYM.sub(0xffae, "unlsn", "Command serial bus to UNLISTEN");
+C64_SYM.sub(0xffab, "untlk", "Command serial bus to UNTALK");
+C64_SYM.sub(0xff8d, "vector", "Read/set vectored I/O");
 
 
 const ENTRY_POINT_OFFSETS: NamedOffset[] = [[C64_COLD_VECTOR_OFFSET, "reset"], [C64_WARM_VECTOR_OFFSET, "nmi"]];
@@ -145,7 +145,7 @@ const C64_8K16K_CART_SNIFFER = new CartSniffer(
           C64_CART_NMI_VECTOR
         ],
         jumpTargetFetcher,
-        C64_KERNAL)
+        C64_SYM)
 );
 
 
