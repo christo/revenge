6c7d4946e6161f081314e4ca301b3d05bcf6e56c
Chris Mountford <chris.mountford@gmail.com>
2025-04-10 16:34:05 +1000

    test 1-gram feature extractor

---
+++ server/test/common/analysis/HistogramFeatureExtractor.test.ts
@@ -0,0 +1,27 @@
+import {expect} from 'chai';
+import {HistogramFeatureExtractor} from "../../../src/common/analysis/HistogramFeatureExtractor";
+
+describe("histogram feature extractor", () => {
+  it("handles simple binary case", () => {
+    const hfe :HistogramFeatureExtractor = new HistogramFeatureExtractor();
+    const buffer = [0, 0, 0, 0, 255];
+    const features = hfe.extract(buffer);
+    const byte255 = features.find(f => f[0] === `byte_255`);
+    expect(byte255).not.to.equal(undefined);
+    const byte0 = features.find(f => f[0] === `byte_0`);
+    expect(byte0).not.to.equal(undefined);
+    // there are more zeroes than 255s
+    expect(byte255[1]).to.be.lt(byte0[1], "there were more zeroes than 255s bro");
+  });
+  it("calculates flat feature vector", () => {
+    const hfe :HistogramFeatureExtractor = new HistogramFeatureExtractor();
+    const twoOfEach = [0, 2, 4, 8, 4, 2, 0, 8];
+    const features = hfe.extract(twoOfEach);
+    const byte2 = features.find(f => f[0] === `byte_2`);
+    expect(byte2).not.to.equal(undefined);
+    const byte0 = features.find(f => f[0] === `byte_0`);
+    expect(byte0).not.to.equal(undefined);
+    // there are more zeroes than 255s
+    expect(byte2[1]).to.equal(byte0[1]);
+  });
+});
