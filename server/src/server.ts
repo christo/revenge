e64260ed3a6c39c82bc73302db95ddc4fa55e924
Chris Mountford <chris.mountford@gmail.com>
2025-04-11 12:15:33 +1000

    wip added method to skip corpus hashing

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,21 +9,25 @@ const PORT = parseInt(process.env.PORT || '3001', 10);
 const app = express();
 
 // set up corpus
-console.log("corpus file collection started");
-const corpus = new Corpus();
-const hashCalc = new HashCalc();
-corpus.files().then(fis => {
-  console.log(`calculating corpus hashes on ${fis.length} files`)
-  Promise.all(fis.map((file: FileInfo) => {
-    const shaP = hashCalc.sha1(fileInfoToFileLike(file));
-    const md5P = hashCalc.md5(fileInfoToFileLike(file));
-    const crc32P = hashCalc.crc32(fileInfoToFileLike(file));
-    return Promise.all([shaP, md5P, crc32P]);
-  })).then(() => {
-    hashCalc.save();
-    console.log("corpus hashes calculated");
+if (process.env.CORPUS_ON_BOOT === 'true') {
+  console.log("corpus file collection started");
+  const corpus = new Corpus();
+  const hashCalc = new HashCalc();
+  corpus.files().then(fis => {
+    console.log(`calculating corpus hashes on ${fis.length} files`)
+    Promise.all(fis.map((file: FileInfo) => {
+      const shaP = hashCalc.sha1(fileInfoToFileLike(file));
+      const md5P = hashCalc.md5(fileInfoToFileLike(file));
+      const crc32P = hashCalc.crc32(fileInfoToFileLike(file));
+      return Promise.all([shaP, md5P, crc32P]);
+    })).then(() => {
+      hashCalc.save();
+      console.log("corpus hashes calculated");
+    });
   });
-})
+} else {
+  console.warn("skipping corpus hash collection because CORPUS_ON_BOOT !== 'true'");
+}
 
 
 app.listen(PORT, () => {
