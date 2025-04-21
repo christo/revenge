8f9de6025fa706c851958558d3f61d32c3fc0d65
Chris Mountford <chris.mountford@gmail.com>
2025-04-21 17:23:09 +1000

    todo integrate client and server in build

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,7 +2,10 @@ import express, {Router} from "express";
 
 const router: Router = express.Router();
 
-/* GET home page. */
+/*
+ * GET home page.
+ * TODO make this work with the frontend build, should be a static serve
+ */
 router.get('/', (_req, res, _next) => {
   res.render('main', {title: 'Revenge'});
   console.log("index get cjm");
