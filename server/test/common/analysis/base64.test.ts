7c02d7d5f05ee291b4f67aaa26c089564ac89540
Chris Mountford <chris.mountford@gmail.com>
2025-04-26 14:50:24 +1000

    add round trip to text test

--- server/test/common/analysis/base64.test.ts
+++ server/test/common/analysis/base64.test.ts
@@ -3,13 +3,17 @@ import {decodeBase64, encodeBase64} from "../../../src/common/analysis/base64";
 
 describe("simple base64", () => {
   it("decodes small text input", () => {
-    const textInput = "this text is input to base64 encoding";
     // obtained from system base64 command
     const expectedBase64 = "dGhpcyB0ZXh0IGlzIGlucHV0IHRvIGJhc2U2NCBlbmNvZGluZw==";
-    const bytes = new TextEncoder().encode(textInput);
-    expect(encodeBase64(Uint8Array.from(bytes))).to.equal(expectedBase64);
+    const inputBytes = new TextEncoder().encode("this text is input to base64 encoding");
+    const encoded = encodeBase64(Uint8Array.from(inputBytes));
+    expect(encoded).to.equal(expectedBase64);
+    // now verify round trip
+    const decoded = decodeBase64(encoded);
+    expect(decoded).to.deep.equal(inputBytes);
+
   });
-  it("does round trip binary literal", () => {
+  it("does binary literal", () => {
     const bytesInput = Uint8Array.from([0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0xba, 0xbe]);
     const encoded = encodeBase64(bytesInput);
     // obtained from system base64 command
