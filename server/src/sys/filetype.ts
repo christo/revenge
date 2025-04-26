5ea67f85f35d7d82907c8fea0ed847e3a9b25b29
Chris Mountford <chris.mountford@gmail.com>
2025-04-26 14:46:44 +1000

    jsdoc

--- server/src/sys/filetype.ts
+++ server/src/sys/filetype.ts
@@ -1,5 +1,9 @@
 import { execFileSync } from "child_process";
 
+/**
+ * Returns the description from the system's file command.
+ * @param filePath must exist
+ */
 function filetype(filePath: string): string {
   const output = execFileSync("file", ["-b", filePath], { encoding: "utf8" });
   return output.trim();
