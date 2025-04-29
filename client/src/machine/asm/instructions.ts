60a64ae163576af9d94bbb8425c50055c0d97939
Chris Mountford <chris.mountford@gmail.com>
2025-04-29 12:15:22 +1000

    unnecessary todo

--- client/src/machine/asm/instructions.ts
+++ client/src/machine/asm/instructions.ts
@@ -303,8 +303,6 @@ class WordDefinitionEdict extends ByteDefinitionEdict {
  */
 class LabelsCommentsOnly extends InstructionBase {
 
-  // TODO add banner macro to dialect so sections can be rendered with multiline instances of this
-
   constructor(lc: LabelsComments) {
     super(lc, SourceType.COMMENT_LABEL);
   }
