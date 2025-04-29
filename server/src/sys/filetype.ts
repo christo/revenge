03dfbcc8b3c0b24a0db171860d7091917af49010
Chris Mountford <chris.mountford@gmail.com>
2025-04-29 12:02:16 +1000

    function to tell if we can use find

--- server/src/sys/filetype.ts
+++ server/src/sys/filetype.ts
@@ -1,12 +1,33 @@
-import { execFileSync } from "child_process";
+import {execFileSync} from "child_process";
+
+/**
+ * Returns the stdout of shell executing `file --version` or null if it fails.
+ * @returns version of the file command or null
+ */
+export function getFileVersion(): string | null {
+  let versionResult: string | null = null;
+  try {
+    // will throw on non-zero exit code
+    execFileSync(`which file`, {shell: true});
+    try {
+      versionResult = execFileSync(`file --version`, {shell: true, encoding: "utf8"}).toString();
+    } catch (e) {
+      console.warn(`error while calling "file --version"`, e);
+    }
+  } catch (e) {
+    console.warn(`Cannot find file command on path`, e);
+  }
+  return versionResult;
+}
+
 
 /**
  * Returns the description from the system's file command.
  * @param filePath must exist
  */
 function filetype(filePath: string): string {
-  const output = execFileSync("file", ["-b", filePath], { encoding: "utf8" });
+  const output = execFileSync("file", ["-b", filePath], {encoding: "utf8"});
   return output.trim();
 }
 
-export { filetype };
+export {filetype};
