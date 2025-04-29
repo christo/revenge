3eb8667161a65ec371d39f766ea9d5562d1feb6c
Chris Mountford <chris.mountford@gmail.com>
2025-04-29 12:05:15 +1000

    todo optionally use file for server-side sniffing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {loadCorpus} from "./loadCorpus.js";
 import quickloadsRouter from "./routes/quickloads.js";
+import {getFileVersion} from "./sys/filetype.js";
 
 const PORT = parseInt(process.env.PORT || '3001', 10);
 
@@ -9,6 +10,14 @@ const app = express();
 loadCorpus().then(() => {
   console.log("corpus loaded");
 });
+
+// TODO incorporate the use of file with sniffers only if it's available
+const fileVersion = getFileVersion();
+if (fileVersion) {
+  console.log(`file version: ${fileVersion}`);
+} else {
+  console.log("file command unavailable");
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
