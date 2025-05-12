0bf6335458fab5b4b98a710686245aaa38db7dad
Chris Mountford <chris.mountford@gmail.com>
2025-05-12 12:45:54 +1000

    add eslint to server

---
+++ server/eslint.config.js
@@ -0,0 +1,27 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+    globalIgnores(["dist/"]),
+
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+    },
+
+    // Spread typescript-eslint configs (array of objects)
+    ...tseslint.configs.recommended,
+
+    {
+        files: ["**/*.{ts,tsx}"],
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+            ],
+        },
+    },
+
+]);
