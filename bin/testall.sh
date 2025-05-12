4f46fd539e116ac283f8e9e83d15cf89217353bd
Chris Mountford <chris.mountford@gmail.com>
2025-05-12 12:45:13 +1000

    add a script for testing both modules

---
+++ bin/testall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# tests both client and server modules for compile, build and automated tests
+# no checkin should fail this
+
+PROJECT_DIR=$(dirname "$0")/..
+
+MODULE="$PROJECT_DIR/server"
+bun --cwd="$MODULE" run compile && bun --cwd="$MODULE" test && bun --cwd="$MODULE" run lint
+MODULE="$PROJECT_DIR/client"
+bun --cwd="$MODULE" run compile && bun --cwd="$MODULE" test && bun --cwd="$MODULE" build && bun --cwd="$MODULE" run lint
