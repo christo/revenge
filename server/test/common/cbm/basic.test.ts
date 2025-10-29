058b821eaf5983d9b2f2cedb9503a6504e65a980
Chris Mountford <chris.mountford@gmail.com>
2025-10-29 17:34:33 +1100

    simplify, reduce deps on Tag

--- server/test/common/cbm/basic.test.ts
+++ server/test/common/cbm/basic.test.ts
@@ -3,23 +3,20 @@ import * as fs from "fs";
 import {CBM_BASIC_2_0} from "../../../src/common/machine/cbm/BasicDecoder.js";
 import {LE, LittleEndian} from "../../../src/common/machine/Endian.js";
 import {FileBlob} from "../../../src/common/machine/FileBlob.js";
-import {LogicalLine} from "../../../src/common/machine/LogicalLine.js";
 import {Memory} from "../../../src/common/machine/Memory.js";
-import {Tag} from "../../../src/common/machine/Tag.js";
 
 describe("basic decoder", () => {
   it("performs simple linear decode", () => {
     const fname = "data/preload/vic20/Killer Comet.prg";
     const buffer = fs.readFileSync(fname);
 
-    let linesRead = 0;
     const fb = FileBlob.fromBytes(fname, Array.from(new Uint8Array(buffer)), LE);
     const fbm = fb.asMemory() as Memory<LittleEndian>;
-    CBM_BASIC_2_0.decode(fbm).getLines().map((ll: LogicalLine) => ll.getTags()).map((t: Tag[]) => t[1]).forEach((_x: Tag) => {
-      linesRead++;
-    })
+    const decoded = CBM_BASIC_2_0.decode(fbm);
+    const linesRead = decoded.getLines().length;
+
     expect(linesRead).equal(38);
-  })
+  });
 });
 
 export {}
